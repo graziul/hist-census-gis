4839cad31b7df0db5262464721d20c0a2514a957
Graziul <cgraziul@ad.brown.edu>
2017-04-24 14:23:55 -0400

    Latest version

--- AssignBlockNums.py
+++ AssignBlockNums.py
@@ -7,14 +7,14 @@ from colorama import AnsiToWin32, init
 r_path = "C:\Program Files\\R\\R-3.3.2\\bin\Rscript"
 script_path = "C:\Users\\cgraziul\\hist-census-gis"
 
-#city_name = "St Louis"
-city_name = "Hartford"
+city_name = "St Louis"
+#city_name = "Hartford"
 city_name = city_name.replace(" ","")
-#state_abbr = "MO"
-state_abbr = "CT"
+state_abbr = "MO"
+#state_abbr = "CT"
 file_path = "S:\Projects\\1940Census\\%s" % (city_name) #TO DO: Directories need to be city_name+state_abbr
-#file_name = file_path + r"\StataFiles_Other\1930\StLouisMO_AutoCleanedV4.csv"
-file_name = file_path + "\\StataFiles_Other\\1930\\HartfordCT_StudAuto.dta"
+file_name = file_path + r"\StataFiles_Other\1930\StLouisMO_AutoCleanedV4.csv"
+#file_name = file_path + "\\StataFiles_Other\\1930\\HartfordCT_StudAuto.dta"
 #file_name = file_path + "\\StataFiles_Other\\1930\\combined_Hartford1930.dta"
 
 cprint("Combined script for automated block numbering (%s)\n" % (city_name), 'yellow',attrs=['bold'], file=AnsiToWin32(sys.stdout))
@@ -22,6 +22,8 @@ cprint("Combined script for automated block numbering (%s)\n" % (city_name), 'ye
 #
 # Step 1: Apply R algorithm (Author: Matt Martinez)
 #
+
+cprint("Step 1: Apply R algorithm\n", attrs=['bold','underline'], file=AnsiToWin32(sys.stdout))
 
 # Assigns block numbers using microdata blocks. Examines proportion of cases that geocode 
 # onto the same physical block and decides  
@@ -58,6 +60,7 @@ if t != 0:
 else:
 	cprint("OK!\n", 'green', file=AnsiToWin32(sys.stdout))
 
+'''
 # Add ranges to new grid
 cprint("Adding ranges to new grid\n", attrs=['bold'], file=AnsiToWin32(sys.stdout))
 t = subprocess.call([r_path,'--vanilla',script_path+'\\blocknum\\R\\Add Ranges to New Grid.R',file_path,city_name,file_name,state_abbr], stdout=open(os.devnull, 'wb'), stderr=open(os.devnull, 'wb'))
@@ -65,6 +68,7 @@ if t != 0:
 	cprint("Error adding ranges to new grid for "+city_name+"\n", 'red', file=AnsiToWin32(sys.stdout))
 else:
 	cprint("OK!\n", 'green', file=AnsiToWin32(sys.stdout))
+'''
 
 # Identify 1930 blocks
 cprint("Identifying 1930 blocks\n", attrs=['bold'], file=AnsiToWin32(sys.stdout))
@@ -82,9 +86,10 @@ else:
 # block descriptions (i.e. streets associated with block in microdata) to make educated
 # guesses about block numbers
 
+cprint("Step 2: Block descriptions from microdata\n", attrs=['bold','underline'], file=AnsiToWin32(sys.stdout))
+
 # Get block description guesses
-cprint("Getting block description guesses\n", attrs=['bold'], file=AnsiToWin32(sys.stdout))
-t = subprocess.call(["python",script_path+"\\blocknum\\Python\\RunBlockDesc.py",file_path,city_name,file_name])
+t = subprocess.call(["python",script_path+"\\blocknum\\Python\\RunBlockDesc.py",file_path,city_name,file_name,state_abbr])
 if t != 0:
 	cprint("Error getting block description guesses for "+city_name+"\n", 'red', file=AnsiToWin32(sys.stdout))
 else:
@@ -97,22 +102,37 @@ else:
 # Assigns block numbers using 1930 block maps. Applies custom built OCR engine 
 # and Matlab toolboxes to produce shapefiles.  
 
-cprint("Runing OCR script\n", attrs=['bold'], file=AnsiToWin32(sys.stdout))
+cprint("Step 3: Runing OCR script\n", attrs=['bold','underline'], file=AnsiToWin32(sys.stdout))
 
-# MOSTLY DONE
 # Run OCR script
 cprint("Runing Matlab script\n", attrs=['bold'], file=AnsiToWin32(sys.stdout))
-t = subprocess.call(["python",script_path+"\\blocknum\\Python\\RunOCR.py",file_path,script_path])
+t = subprocess.call(["python",script_path+"\\blocknum\\Python\\RunOCR.py",file_path,script_path],stdout=open(os.devnull, 'wb'))
 if t != 0:
 	cprint("Error running Matlab OCR script for "+city_name+"\n", 'red', file=AnsiToWin32(sys.stdout))
 else:
 	cprint("OK!\n", 'green', file=AnsiToWin32(sys.stdout))
 
-# UNTESTED BUT SHOULD BE DONE
 # Integrate OCR block numbering results
 cprint("Integrating OCR block numbering results\n", attrs=['bold'], file=AnsiToWin32(sys.stdout))
-t = subprocess.call(["python",script_path+"\\blocknum\\Python\\MapOCRintegration.py",file_path,file_name])
+t = subprocess.call(["python",script_path+"\\blocknum\\Python\\MapOCRintegration.py",file_path,city_name,file_name])
 if t != 0:
 	cprint("Error integrating OCR block numbering results for "+city_name+"\n", 'red', file=AnsiToWin32(sys.stdout))
+else:
+	cprint("OK!\n", 'green', file=AnsiToWin32(sys.stdout))
+
+#
+# Step 4: Set confidence
+#
+
+# Condenses automated block numbering into one column of best guesses with confidence
+# for each guess and as few other image columns as possible  
+
+cprint("Step 4: Set confidence\n", attrs=['bold','underline'], file=AnsiToWin32(sys.stdout))
+
+# Integrate OCR block numbering results
+cprint("Setting confidence\n", attrs=['bold'], file=AnsiToWin32(sys.stdout))
+t = subprocess.call(["python",script_path+"\\blocknum\\Python\\SetConfidence.py",file_path,file_name])
+if t != 0:
+	cprint("Error setting confidence for for "+city_name+"\n", 'red', file=AnsiToWin32(sys.stdout))
 else:
 	cprint("OK!\n", 'green', file=AnsiToWin32(sys.stdout))
