80da0baab62a51c465d6fec0de147e8daaa36b76
graziul <christopher.grazuil@gmail.com>
2017-12-19 13:22:11 -0500

    Simplified dependencies

--- blocknum/Python/RunBlockDesc.py
+++ blocknum/Python/RunBlockDesc.py
@@ -6,8 +6,6 @@ import pysal as ps
 import csv
 import time
 import pickle
-from termcolor import colored, cprint
-from colorama import AnsiToWin32, init
 from multiprocessing.dummy import Pool 
 from _functools import partial
 
@@ -19,11 +17,7 @@ city_path = sys.argv[1]
 city_name = sys.argv[2]
 state_abbr = sys.argv[3]
 
-#city_path = "S:\\Projects\\1940Census\\StLouis"
-#city_name = "StLouis"
-#state_abbr = "MO"
-
-gis_path = city_path + "\\GIS_edited\\"
+gis_path = city_path + "/GIS_edited/"
 block_file = gis_path + city_name + "_1930_Block_Choice_Map.shp"
 pblk_file = gis_path + city_name + "_1930_Pblk.shp"
 #stgrid_file = gis_path + city_name + state_abbr + "_1930_stgrid_edit.shp"
@@ -32,9 +26,9 @@ pblk_grid_file = gis_path + city_name + state_abbr + "_1930_Pblk_Grid_SJ.shp"
 out_file = gis_path + city_name + "_1930_Block_Choice_Map2.shp"
 pblk_grid_dict_file = gis_path + city_name + "_pblk_grid_dict.pkl"
 pblk_st_dict_file = gis_path + city_name + "_pblk_st_dict.pkl"
-microdata_file = city_path + "\\StataFiles_Other\\1930\\" + city_name + state_abbr + "_StudAuto.dta"
+microdata_file = city_path + "/StataFiles_Other/1930/" + city_name + state_abbr + "_StudAuto.dta"
 
-cprint("Getting block description guesses\n", attrs=['bold'], file=AnsiToWin32(sys.stdout))
+print("Getting block description guesses\n")
 
 #
 # Step 1: Create "block descriptions" using microdata
@@ -78,7 +72,7 @@ df_pre = df_pre[['ed','block',street]]
 
 end = time.time()
 run_time = round(float(end-start)/60, 1)
-cprint("Finished loading microdata (took " + str(run_time) + " minutes)", 'cyan', file=AnsiToWin32(sys.stdout))
+print("Finished loading microdata (took " + str(run_time) + " minutes)", 'cyan')
 
 #TO DO: Clean up block numbers
 df_pre = df_pre[df_pre['block'].notnull() & df_pre['ed'].notnull()]
@@ -99,7 +93,7 @@ df['block_addresses'] = df.apply(lambda x: block_num_addresses_dict[x['ed_block'
 df['prop_block'] = df['st_addresses']/df['block_addresses']
 micro_blocks_dict = {group:streets[street].values.tolist() for group, streets in df.groupby('ed_block')}
 	#TO DO: Remove blocks defined by a single street (too indeterminate)???
-cprint("Finished building microdata block-street dictionary", 'cyan', file=AnsiToWin32(sys.stdout))
+print("Finished building microdata block-street dictionary")
 
 #
 # Step 1.5: Spatial join stgrid to pblk to get {pblk_id:[FULLNAME]} and {pblk_id:[grid_id]}
@@ -128,7 +122,7 @@ pickle.dump(pblk_grid_dict,open(pblk_grid_dict_file,'wb'))
 
 end = time.time()
 run_time = round(float(end-start)/60, 1)
-cprint("Finished overlaying physical blocks and street grid (took " + str(run_time) + " minutes)"+"\n", 'cyan', file=AnsiToWin32(sys.stdout))
+print("Finished overlaying physical blocks and street grid (took " + str(run_time) + " minutes)"+"\n")
 
 #
 # Step 2: Now use empiric block descriptions to try to label physical blocks 
@@ -146,7 +140,7 @@ def extract_step1_labels(fields):
 					pblks_labeled1.append(row[0])
 	num_labels_step1 = len(labels_step1)
 	per_micro_blocks = round(100*float(num_labels_step1)/num_micro_blocks, 1)
-	cprint("Physical blocks labeled in Step 1: "+str(num_labels_step1)+" ("+str(per_micro_blocks)+r"% of microdata blocks)"+"\n",'green',attrs=['bold'], file=AnsiToWin32(sys.stdout))
+	print("Physical blocks labeled in Step 1: "+str(num_labels_step1)+" ("+str(per_micro_blocks)+r"% of microdata blocks)"+"\n")
 	return labels_step1, pblks_labeled1, num_labels_step1
 
 #This step determines which ED-blocks to consider labeled based on prior numbering efforts
@@ -183,8 +177,8 @@ def match_using_block_desc(pblks_labeled):
 	per_micro_blocks = round(100*float(num_matches)/num_micro_blocks, 1)
 	end = time.time()
 	run_time = round(float(end-start)/60, 1)
-	cprint("Matches based on block description: "+str(num_matches)+" ("+str(per_micro_blocks)+r"% of microdata blocks)",file=AnsiToWin32(sys.stdout))
-	cprint("Matching took "+str(run_time)+" minutes to complete\n",'cyan',file=AnsiToWin32(sys.stdout))
+	print("Matches based on block description: "+str(num_matches)+" ("+str(per_micro_blocks)+r"% of microdata blocks)")
+	print("Matching took "+str(run_time)+" minutes to complete\n")
 	return temp_match_dict, list(temp_pblks_labeled), list(temp_labels), num_matches
 
 thresh = 1
@@ -197,11 +191,11 @@ good_match_dict, pblks_labeled_good, labels_good, num_good_matches = match_using
 
 num_labeled_step2 = num_exact_matches+num_good_matches
 per_micro_blocks = round(100*float(num_labeled_step2)/num_micro_blocks, 1)
-cprint("\n"+"Physical blocks labeled by Step 2: "+str(num_labeled_step2)+" ("+str(per_micro_blocks)+r"% of microdata blocks)"+"\n",'green',attrs=['bold'],file=AnsiToWin32(sys.stdout))
+print("\n"+"Physical blocks labeled by Step 2: "+str(num_labeled_step2)+" ("+str(per_micro_blocks)+r"% of microdata blocks)"+"\n")
 
 num_labeled_steps1and2 = num_labeled_step1+num_labeled_step2
 per_micro_blocks = round(100*float(num_labeled_steps1and2)/num_micro_blocks, 1)
-cprint("Physical blocks labeled by Step 1 and Step 2: "+str(num_labeled_steps1and2)+" ("+str(per_micro_blocks)+r"% of microdata blocks)"+"\n",attrs=['bold'], file=AnsiToWin32(sys.stdout))
+print("Physical blocks labeled by Step 1 and Step 2: "+str(num_labeled_steps1and2)+" ("+str(per_micro_blocks)+r"% of microdata blocks)"+"\n")
 
 #Add labels 
 
