d2509a0dfd7d1d568009d7613bd130ba413da4e6
graziul <christopher.grazuil@gmail.com>
2018-04-03 14:12:18 -0400

    Fixed issue with numeric vs. factor ED variable

--- blocknum/R/Identify 1930 EDs.R
+++ blocknum/R/Identify 1930 EDs.R
@@ -53,8 +53,10 @@ trim <- function( x ) {
   
 #Adds a variable with the number 1 used to enumerate values throughout code
   EDs$one<-car::recode(EDs$ed,"\" \"=0; else=1")
+  if (is.factor(EDs$one)==TRUE) {
   EDs$one<-as.numeric(levels(EDs$one))[EDs$one]
-
+  }
+  
 #Remove pblk_id with '0';
   EDs<-EDs[which(EDs$pblk_id!=0),]
   
