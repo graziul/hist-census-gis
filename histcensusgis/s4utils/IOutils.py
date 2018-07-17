15a24c9eb2e82064229c11c465f793a7974d1608
akisch <amkisch@gmail.com>
2018-07-16 22:10:44 -0400

    Update IOutils.py

--- histcensusgis/s4utils/IOutils.py
+++ histcensusgis/s4utils/IOutils.py
@@ -17,7 +17,7 @@ def get_paths(city_info, data_path="S:/Projects/1940Census/", r_path="C:/Program
 	city_name = city_name.replace(' ','')
 
 	city_state = city_name + state_abbr
-	if city_state in ['KansasCityKS','KansasCityKS','RichmondVA','RichmondNY']:
+	if city_state in ['KansasCityKS','KansasCityMO','RichmondVA','RichmondNY']:
 		dir_path = data_path + city_state
 	else:
 		dir_path = data_path + city_name #TO DO: Directories need to be city_name+state_abbr
